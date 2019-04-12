e60ccab57548942049b703945cbacb6ea4c91c83
gdj_porg <x@desde.porg.com>
2019-04-12 12:35:15 -0500

    fixed discretization issue; took rows as data.frames, and returned all samples with same discrete value

--- libs/functions_mi.R
+++ libs/functions_mi.R
@@ -20,23 +20,42 @@ library("readr")
 #discretization
 
 discretizer <- function(expmatrix, diskz = "equalfreq"){
+  #names 
+  featureNames <- dplyr::pull(expmatrix, 1)
+  sampleNames  <- colnames(expmatrix[,-1])
+  
   my_index <- 1:nrow(expmatrix)
-  names(my_index) <- dplyr::pull(expmatrix, 1)
+  #names(my_index) <- dplyr::pull(expmatrix, 1)
+  names(my_index) <- featureNames
+  
   my_discrete <- lapply(X = my_index, FUN = function(i){
-    my_row <- expmatrix[i,-1]
+    my_row <- expmatrix[i,-1] %>% as.numeric()
+    names(my_row) <- sampleNames
     my_row <- infotheo::discretize(X = my_row, disc = diskz)
-    
+    my_row <- as.data.frame(t(my_row))
+    colnames(my_row) <- sampleNames
+    return(my_row)
     
   })
   return(my_discrete)
 }
 
 par_discretizer <- function(expmatrix, korez = 2, diskz = "equalfreq"){
+  #names 
+  featureNames <- dplyr::pull(expmatrix, 1)
+  sampleNames  <- colnames(expmatrix[,-1])
+  
   my_index <- 1:nrow(expmatrix)
-  names(my_index) <- dplyr::pull(expmatrix, 1)
+  #names(my_index) <- dplyr::pull(expmatrix, 1)
+  names(my_index) <- featureNames
+  
   my_discrete <- parallel::mclapply(X = my_index, mc.cores = korez, FUN = function(i){
-    my_row <- expmatrix[i,-1]
+    my_row <- expmatrix[i,-1] %>% as.numeric()
+    names(my_row) <- sampleNames
     my_row <- infotheo::discretize(X = my_row, disc = diskz)
+    my_row <- as.data.frame(t(my_row))
+    colnames(my_row) <- sampleNames
+    return(my_row)
     
   })
   return(my_discrete)
